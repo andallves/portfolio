ac670878c08f29701932cc1e66800e34b2f96a15
andallves <andrealves10a@gmail.com>
2024-03-10 14:44:38 -0300

    ✨ feat: create logo component

---
+++ portfolio/src/app/core/components/logo/logo.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-logo',
+  standalone: true,
+  imports: [NgOptimizedImage, RouterLink],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.scss'
+})
+export class LogoComponent {
+  @Input() link = '';
+}
