b4dae3c8d3160a707a6b3fe404389d05cd0f55d6
andallves <andrealves10a@gmail.com>
2024-03-15 20:36:10 -0300

    :sparkles: feat: create button-secondary component

---
+++ portfolio/src/app/shared/components/button-secondary/button-secondary.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { NgOptimizedImage, NgTemplateOutlet } from '@angular/common';
+import { Buttontype } from '../../../core/models/entities/button';
+import { Icon } from '../../../core/models/entities/icon';
+
+@Component({
+  selector: 'app-button-secondary',
+  standalone: true,
+  imports: [NgTemplateOutlet, NgOptimizedImage],
+  templateUrl: './button-secondary.component.html',
+  styleUrl: './button-secondary.component.scss',
+})
+export class ButtonSecondaryComponent {
+  @Input() type: Buttontype = 'button';
+  @Input({ required: true }) buttonText = '';
+  @Input() icon!: Icon;
+}
