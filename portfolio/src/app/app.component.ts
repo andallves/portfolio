b8745f06b4fa98d023160ee0b8629c5c6290ea08
andallves <andrealves10a@gmail.com>
2024-03-15 22:18:55 -0300

    :sparkles: feat: add new components

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/layouts/header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from '#core/layouts/header/header.component';
+import { CustomTextComponent } from '#shared/components/custom-text/custom-text.component';
+import { AboutMeComponent } from '#core/layouts/about-me/about-me.component';
+import { ProjectsComponent } from '#core/layouts/projects/projects.component';
+import { EmphasisComponent } from '#core/layouts/emphasis/emphasis.component';
+import { TechnologiesComponent } from '#core/layouts/technologies/technologies.component';
+import { FooterComponent } from '#core/layouts/footer/footer.component';
+import { CareerComponent } from '#core/layouts/career/career.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    CustomTextComponent,
+    MatIconModule,
+    AboutMeComponent,
+    ProjectsComponent,
+    EmphasisComponent,
+    TechnologiesComponent,
+    FooterComponent,
+    CareerComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
