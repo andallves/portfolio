6086b969d92cb757b0df59f24795b22517d86e01
andallves <andrealves10a@gmail.com>
2024-03-18 22:28:58 -0300

    :sparkles: feat: add animation

--- portfolio/src/app/shared/components/image/image.component.ts
+++ portfolio/src/app/shared/components/image/image.component.ts
@@ -1,14 +1,25 @@
 import { Component, Input } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Image } from '#core/models/entities/image';
 
 @Component({
   selector: 'app-image',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, CommonModule],
   templateUrl: './image.component.html',
   styleUrl: './image.component.scss',
 })
 export class ImageComponent {
   @Input() image!: Image;
+  protected animated = false;
+  public animate(): void {
+    this.animated = true;
+    this.time();
+  }
+
+  public time() {
+    setTimeout(() => {
+      this.animated = false;
+    }, 2000);
+  }
 }
