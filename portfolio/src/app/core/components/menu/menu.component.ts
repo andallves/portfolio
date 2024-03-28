2a2234ef6896d96aa40557d7666c22f4dbd01c29
andallves <andrealves10a@gmail.com>
2024-03-28 13:10:14 -0300

    :sparkles: feat: update

--- portfolio/src/app/core/components/menu/menu.component.ts
+++ portfolio/src/app/core/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LogoComponent } from '#core/components/logo/logo.component';
 import { SocialMediaComponent } from '#shared/components/social-media/social-media.component';
@@ -10,4 +10,9 @@ import { SocialMediaComponent } from '#shared/components/social-media/social-med
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
-export class MenuComponent {}
+export class MenuComponent {
+  @ViewChild('skills') pageSkills!: ElementRef;
+  goToSkills() {
+    this.pageSkills.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
+}
