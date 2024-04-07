6957dca39381f716da8d88b5a5bdbfb0b1597728
andallves <andrealves10a@gmail.com>
2024-04-07 19:39:15 -0300

    :bug: fix: fix bug in mobile

--- src/app/core/models/entities/project-card.ts
+++ src/app/core/models/entities/project-card.ts
@@ -1,7 +1,14 @@
 import { Image } from '../../models/entities/image';
 import { CustomText } from '../../models/entities/custom-text';
 
+export enum Stack {
+  frontend = 'frontend',
+  backend = 'backend',
+  fullstack = 'fullstack',
+}
+
 export interface ProjectCard {
+  stack: Stack;
   title: string;
   description: string;
   tecnologias: CustomText[];
