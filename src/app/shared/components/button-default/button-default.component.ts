5f1994290142cb41d1e65ea8ab81a03d84fdecff
andallves <andrealves10a@gmail.com>
2024-04-09 23:45:47 -0300

    :wrench: refactor: rename attributes

--- src/app/shared/components/button-default/button-default.component.ts
+++ src/app/shared/components/button-default/button-default.component.ts
@@ -5,26 +5,31 @@ import {
   Input,
   Output,
 } from '@angular/core';
-import { NgClass, NgOptimizedImage, NgTemplateOutlet } from '@angular/common';
+import {
+  NgClass,
+  NgIf,
+  NgOptimizedImage,
+  NgTemplateOutlet,
+} from '@angular/common';
 import { Buttontype } from '#core/models/entities/button';
 import { Icon } from '#core/models/entities/icon';
 
 @Component({
   selector: 'app-button-default',
   standalone: true,
-  imports: [NgTemplateOutlet, NgOptimizedImage, NgClass],
+  imports: [NgTemplateOutlet, NgOptimizedImage, NgClass, NgIf],
   templateUrl: './button-default.component.html',
   styleUrl: './button-default.component.scss',
 })
 export class ButtonDefaultComponent {
   @Input({ transform: booleanAttribute }) isDark = false;
-  @Input() url?: string | null = null;
-  @Input() type: Buttontype = 'button';
+  @Input() buttonUrl?: string = '';
+  @Input() buttonType: Buttontype = 'button';
   @Input({ required: true }) buttonText = '';
-  @Input() icon?: Icon;
-  @Output() clickButton: EventEmitter<boolean> = new EventEmitter();
+  @Input() buttonIcon?: Icon;
+  @Output() buttonClick: EventEmitter<boolean> = new EventEmitter();
 
   public clicked(): void {
-    this.clickButton.emit();
+    this.buttonClick.emit();
   }
 }
