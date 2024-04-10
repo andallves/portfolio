d7b62829a479a44ba181040cc23d1255ccf35eb8
andallves <andrealves10a@gmail.com>
2024-04-09 21:22:09 -0300

    :feat: sparkles: implement dark layout in the default button

--- src/app/shared/components/button-default/button-default.component.ts
+++ src/app/shared/components/button-default/button-default.component.ts
@@ -1,19 +1,27 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { NgOptimizedImage, NgTemplateOutlet } from '@angular/common';
+import {
+  booleanAttribute,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { NgClass, NgOptimizedImage, NgTemplateOutlet } from '@angular/common';
 import { Buttontype } from '#core/models/entities/button';
 import { Icon } from '#core/models/entities/icon';
 
 @Component({
   selector: 'app-button-default',
   standalone: true,
-  imports: [NgTemplateOutlet, NgOptimizedImage],
+  imports: [NgTemplateOutlet, NgOptimizedImage, NgClass],
   templateUrl: './button-default.component.html',
   styleUrl: './button-default.component.scss',
 })
 export class ButtonDefaultComponent {
+  @Input({ transform: booleanAttribute }) isDark = false;
+  @Input() url?: string | null = null;
   @Input() type: Buttontype = 'button';
   @Input({ required: true }) buttonText = '';
-  @Input({ required: true }) icon!: Icon;
+  @Input() icon?: Icon;
   @Output() clickButton: EventEmitter<boolean> = new EventEmitter();
 
   public clicked(): void {
