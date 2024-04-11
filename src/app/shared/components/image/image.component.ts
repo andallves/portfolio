0fb2974ca5bfdc2f90ec4a2aeb6f08eda4d3a0fd
andallves <andrealves10a@gmail.com>
2024-04-11 00:10:15 -0300

    :wrench: refactor: remove image click and ajust responsivity

--- src/app/shared/components/image/image.component.ts
+++ src/app/shared/components/image/image.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Image } from '#core/models/entities/image';
-import { ScrollClassDirective } from '#core/directives/scroll-class.directive';
 
 @Component({
   selector: 'app-image',
   standalone: true,
-  imports: [NgOptimizedImage, CommonModule, ScrollClassDirective],
+  imports: [NgOptimizedImage, CommonModule],
   templateUrl: './image.component.html',
   styleUrl: './image.component.scss',
 })
 export class ImageComponent {
   @Input({ required: true }) image!: Image;
+  protected isAnimated = false;
 }
