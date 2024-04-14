66ef013741f7948afbeccbfea06ffad9a3d6293b
andreallves <andreallves.dev@gmail.com>
2024-04-14 17:18:21 -0300

    :wrench: refactor: rename carrer to trajectory and implements styles

--- src/app/core/layouts/trajectory/trajectory.component.ts
+++ src/app/core/layouts/trajectory/trajectory.component.ts
@@ -5,23 +5,48 @@ import { LoadingComponent } from '#shared/components/loading/loading.component';
 import { carrerIcon } from '#core/data/icons';
 import { CardTrajectory } from '#core/models/entities/trajectory';
 import { TrajectoryCardComponent } from './trajectory-card/trajectory-card.component';
-import { academicTrajectory, carrerTrajectory } from '#core/data/trajectory';
-import { ScrollClassDirective } from '#core/directives/scroll-class.directive';
 
 @Component({
   selector: 'app-trajectory',
   standalone: true,
-  imports: [
-    HeadingComponent,
-    LoadingComponent,
-    TrajectoryCardComponent,
-    ScrollClassDirective,
-  ],
+  imports: [HeadingComponent, LoadingComponent, TrajectoryCardComponent],
   templateUrl: './trajectory.component.html',
   styleUrl: './trajectory.component.scss',
 })
 export class TrajectoryComponent {
   carrerIcon: Icon = carrerIcon;
-  carrerTrajectory: Array<CardTrajectory> = carrerTrajectory;
-  academicTrajectory: Array<CardTrajectory> = academicTrajectory;
+
+  carrerTrajectory: Array<CardTrajectory> = [
+    {
+      trajectoryTitle: 'Núcleo de Desenvolvimento de Software',
+      trajectoryDescription:
+        'Atuo desenvolendo sistema no Núcle de Desenvolvimento de Software para a comunidade academica do IFCE campus Maracanaú.',
+      trajectoryTime: '6 meses',
+      trajectoryDate: 'Agosto/2023 - Atualmente',
+    },
+    {
+      trajectoryTitle: 'Ciência da Computação',
+      trajectoryDescription:
+        'Atuo desenvolendo sistema no Núcle de Desenvolvimento de Software para a comunidade academica do IFCE campus Maracanaú.',
+      trajectoryTime: '6 meses',
+      trajectoryDate: 'Agosto/2023 - Atualmente',
+    },
+  ];
+
+  academicTrajectory: Array<CardTrajectory> = [
+    {
+      trajectoryTitle: 'Ciência da Computação',
+      trajectoryDescription:
+        'Atuo desenvolendo sistema no Núcle de Desenvolvimento de Software para a comunidade academica do IFCE campus Maracanaú.',
+      trajectoryTime: '6 meses',
+      trajectoryDate: 'Agosto/2023 - Atualmente',
+    },
+    {
+      trajectoryTitle: 'Ciência da Computação',
+      trajectoryDescription:
+        'Atuo desenvolendo sistema no Núcle de Desenvolvimento de Software para a comunidade academica do IFCE campus Maracanaú.',
+      trajectoryTime: '6 meses',
+      trajectoryDate: 'Agosto/2023 - Atualmente',
+    },
+  ];
 }
