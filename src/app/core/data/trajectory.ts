3608fd6afc9f2c10558e4844168093e9377a0465
Andre Alves <37510133+andallves@users.noreply.github.com>
2024-04-16 10:16:36 -0300

    Pr 13 desenvolver tela de experiencias (#15)
    * :wrench: refactor: remove data to data folder
    
    * :wrench: refactor: ajust layout
    
    * :sparkles: feat: create a back to top button component
    
    * :wrench: implements buttons functionalities
    
    * :hammer: build: update
    
    * :wrench: refactor: using html sematic
    
    * :bug: fix: ajust hover effect and change background

---
+++ src/app/core/data/trajectory.ts
@@ -0,0 +1,42 @@
+import { CardTrajectory } from '#core/models/entities/trajectory';
+
+export const carrerTrajectory: Array<CardTrajectory> = [
+  {
+    trajectoryTitle: 'Bolsista | Núcleo de Desenvolvimento de Software',
+    trajectoryDescription:
+      'Como desenvolvedor front-end no NDS, contribuo para aplicações da comunidade acadêmica do IFCE campus Maracanaú.',
+    trajectoryTime: '9 meses',
+    trajectoryDate: 'Agosto/2023 - Atualmente',
+  },
+];
+
+export const academicTrajectory: Array<CardTrajectory> = [
+  {
+    trajectoryTitle: 'Ciência da Computação',
+    trajectoryDescription:
+      'Atualmente sou estudante de Ciência da Computação no Instituto Federal de Educação, Ciência e Tecnologia do Ceará.',
+    trajectoryTime: '',
+    trajectoryDate: 'Outubro/2018 - Atualmente',
+  },
+  {
+    trajectoryTitle: 'Curso Desenvolvedor Web | Udemy',
+    trajectoryDescription:
+      'HTML5, CSS3, Javascript, BootStrap, jQuery, TypeScript, PHP, MySQL, AJAX, SEO e muito mais.',
+    trajectoryTime: '49 horas',
+    trajectoryDate: 'Maio/2022 - Setembro/2022',
+  },
+  {
+    trajectoryTitle: 'Curso de JavaScript e TypeScript | Udemy',
+    trajectoryDescription:
+      'Javascript e TypeScript - front-end e back-end (Full Stack) - Node, Express, noSQL, React, hooks, Redux, Design Patterns.',
+    trajectoryTime: '146 horas',
+    trajectoryDate: 'Novembro/2022 - Junho/2023',
+  },
+  {
+    trajectoryTitle: 'Curso de React.Js e Next.Js | Udemy',
+    trajectoryDescription:
+      'ReactJS, NextJS, Styled-Components, Testes com Jest , Storybook, Strapi, HTML e CSS com TypeScript e JavaScript.',
+    trajectoryTime: '92 horas',
+    trajectoryDate: 'Abril/2023 - Setembro/2023',
+  },
+];
---
+++ src/app/shared/components/back-to-top/back-to-top.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { ButtonSecondaryComponent } from '../button-secondary/button-secondary.component';
+import { Icon } from '#core/models/entities/icon';
+import { upArrow } from '#core/data/icons';
+
+@Component({
+  selector: 'app-back-to-top',
+  standalone: true,
+  imports: [ButtonSecondaryComponent],
+  templateUrl: './back-to-top.component.html',
+  styleUrl: './back-to-top.component.scss',
+})
+export class BackToTopComponent implements OnInit {
+  showButton = false;
+  upArrow: Icon = upArrow;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.renderer.listen('window', 'scroll', () => {
+      this.showButton = window.scrollY > 100;
+    });
+  }
+
+  backToTop() {
+    window.scroll({ top: 0, behavior: 'smooth' });
+  }
+}
