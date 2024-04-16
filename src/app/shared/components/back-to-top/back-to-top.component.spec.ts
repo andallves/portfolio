8a009ddd6f634a4e3d59184f1c90683fb8cb1860
Andre Alves <37510133+andallves@users.noreply.github.com>
2024-04-16 10:16:36 -0300

    Pr 13 desenvolver tela de experiencias (#15)
    * :wrench: refactor: remove data to data folder
    
    * :wrench: refactor: ajust layout
    
    * :sparkles: feat: create a back to top button component
    
    * :wrench: implements buttons functionalities
    
    * :hammer: build: update
    
    * :wrench: refactor: using html sematic
    
    * :bug: fix: ajust hover effect and change background

--- src/app/shared/components/back-to-top/back-to-top.component.spec.ts
+++ src/app/shared/components/back-to-top/back-to-top.component.spec.ts
@@ -8,8 +8,9 @@ describe('BackToTopComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BackToTopComponent],
-    }).compileComponents();
+      imports: [BackToTopComponent]
+    })
+    .compileComponents();
     
     fixture = TestBed.createComponent(BackToTopComponent);
     component = fixture.componentInstance;
