86af58aa84fb5248fcd3e10d52b77521fb153705
Andre Alves <37510133+andallves@users.noreply.github.com>
2024-04-17 20:15:50 -0300

    Development (#20)
    * :sparkles: feat: implement link in the menu button
    
    * :bug: fix: responsivity
    
    * :sparkles: feat: ajust responsivity
    
    * :wrench: refactor: extract all icon to diferent file
    
    * :wrench: refactor: remove image click and ajust responsivity
    
    * :sparkles: feat: implements button funcionality to redirect to whatsapp
    
    * :bug: fix: removing unnecessary spacing between carousel and button see all
    
    * :bug: fix: add correct phone number
    
    * :sparkles: feat: implements loading
    
    * :wrench: refactor: rename curriculum and change whatsapp message
    
    * :wrench: refactor: add loading component
    
    * :wrench: refactor: change the animation section to component layout instead app component
    
    * :wrench: refactor: extract icons to icons.ts file
    
    * :wrench: refactor: update
    
    * :sparkles: feat: implements shadow in the text
    
    * :sparkles: feat: implements light theme in the heading
    
    * :wrench: refactor: change font-family
    
    * :wrench: refactor: update text and ajust responsivity
    
    * :wrench: refactor: update transitions
    
    * :wrench: refactor: ajust responsivity
    
    * sparkles: feat: add tech image
    
    * :wrench: feat: replace variable font-sizes by font-size
    
    * :wrench: refactor: ajust font-weight
    
    * :sparkles: feat: add style in the technology icon
    
    * :wrench: refactor ajust responsivity
    
    * :wrench: refactor: improve paragraph text
    
    * :sparkles: feat: add new technologies
    
    * :wrench: refactor: change divider color and opacity
    
    * :wrench: refactor: remove href from button-secondary
    
    * :wrench: refactor: change background color
    
    * :wrench: refactor: rename carrer to trajectory and implements styles
    
    * :sparkles: feat: create trajectory card
    
    * :wrench: refactor: change background-color
    
    * :bug: fix: ajust layout
    
    * :wrench: refactor add trajectory component
    
    * :wrench: refactor: ajust card height
    
    * :wrench: refactor: change hover effect
    
    * :wrench: refactor: remove comments and ajust box-shadow
    
    * :wrench: refactor: ajust icon size
    
    * :sparkles: feat: add scale effect
    
    * :wrench: refactor: remove data to data folder
    
    * :wrench: refactor: ajust layout
    
    * :sparkles: feat: create a back to top button component
    
    * :wrench: implements buttons functionalities
    
    * :hammer: build: update
    
    * :wrench: refactor: using html sematic
    
    * :bug: fix: ajust hover effect and change background
    
    * Pr 13 desenvolver tela de experiencias (#15)
    
    * :wrench: refactor: remove data to data folder
    
    * :wrench: refactor: ajust layout
    
    * :sparkles: feat: create a back to top button component
    
    * :wrench: implements buttons functionalities
    
    * :hammer: build: update
    
    * :wrench: refactor: using html sematic
    
    * :bug: fix: ajust hover effect and change background
    
    * :hammer: build: update
    
    * :hammer: build: update
    
    * 🔩 chore: update .gitignore

--- src/app/core/layouts/contact/contact.component.ts
+++ src/app/core/layouts/contact/contact.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { HeadingComponent } from '#shared/components/heading/heading.component';
 import { ButtonDefaultComponent } from '#shared/components/button-default/button-default.component';
-import { ButtonSecondaryComponent } from '#shared/components/button-secondary/button-secondary.component';
 import { Icon } from '#core/models/entities/icon';
 import { LoadingComponent } from '#shared/components/loading/loading.component';
-import {
-  contactIcon,
-  emailIcon,
-  upArrow,
-  whatsappIcon,
-} from '#core/data/icons';
+import { contactIcon, emailIcon, whatsappIcon } from '#core/data/icons';
+import { BackToTopComponent } from '#shared/components/back-to-top/back-to-top.component';
 
 @Component({
   selector: 'app-contact',
@@ -17,7 +12,7 @@ import {
   imports: [
     HeadingComponent,
     ButtonDefaultComponent,
-    ButtonSecondaryComponent,
+    BackToTopComponent,
     LoadingComponent,
   ],
   templateUrl: './contact.component.html',
@@ -27,5 +22,4 @@ export class ContactComponent {
   contactIcon: Icon = contactIcon;
   whatsappIcon: Icon = whatsappIcon;
   emailIcon: Icon = emailIcon;
-  upArrow: Icon = upArrow;
 }
