eae1a85025e3f3acc1588e8f749ecc1cff78a36d
andreallves <andreallves.dev@gmail.com>
2024-04-18 16:02:49 -0300

    :wrench: refactor: add animation in elements

--- src/app/core/layouts/trajectory/trajectory.component.ts
+++ src/app/core/layouts/trajectory/trajectory.component.ts
@@ -6,11 +6,17 @@ import { carrerIcon } from '#core/data/icons';
 import { CardTrajectory } from '#core/models/entities/trajectory';
 import { TrajectoryCardComponent } from './trajectory-card/trajectory-card.component';
 import { academicTrajectory, carrerTrajectory } from '#core/data/trajectory';
+import { ScrollClassDirective } from '#core/directives/scroll-class.directive';
 
 @Component({
   selector: 'app-trajectory',
   standalone: true,
-  imports: [HeadingComponent, LoadingComponent, TrajectoryCardComponent],
+  imports: [
+    HeadingComponent,
+    LoadingComponent,
+    TrajectoryCardComponent,
+    ScrollClassDirective,
+  ],
   templateUrl: './trajectory.component.html',
   styleUrl: './trajectory.component.scss',
 })
