35044b05fb1581caee00adc07495b121c45a5c48
Andre Alves <andrealves10a@gmail.com>
2025-11-27 15:11:50 -0300

    refactor: refatorando

--- src/app/features/career/career.component.ts
+++ src/app/features/career/career.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { Icon } from '@core/models/entities/icon';
 import { HeadingComponent } from '@shared/components/heading/heading.component';
 import { carrerIcon } from '@core/data/icons';
-import { CardTrajectory, Experience } from '@core/models/entities/trajectory';
-import { academicTrajectory, experiencesData } from '@core/data/experiences';
+import { Experience } from '@core/models/entities/trajectory';
+import { experiencesData } from '@core/data/experiences';
 import { CareerTableComponent } from 'src/app/features/career/components/career-table/career-table.component';
 import { ScrollClassDirective } from '@core/directives/scroll-class.directive';
 
@@ -17,7 +17,6 @@ import { ScrollClassDirective } from '@core/directives/scroll-class.directive';
 export class CareerComponent {
   carrerIcon: Icon = carrerIcon;
   experiences: Experience[] = [];
-  academicTrajectory: Array<CardTrajectory> = academicTrajectory;
 
   constructor() {
     this.experiences = experiencesData;
