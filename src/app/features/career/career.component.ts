fc965c3c0899e752b873078ddd130770fe77668c
Andre Alves <37510133+andallves@users.noreply.github.com>
2025-11-28 09:59:13 -0300

    fix: resolvendo erro na animação do career-section (#40)

--- src/app/features/career/career.component.ts
+++ src/app/features/career/career.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Icon } from '@core/models/entities/icon';
 import { HeadingComponent } from '@shared/components/heading/heading.component';
 import { carrerIcon } from '@core/data/icons';
@@ -6,6 +6,7 @@ import { Experience } from '@core/models/entities/trajectory';
 import { experiencesData } from '@core/data/experiences';
 import { CareerTableComponent } from 'src/app/features/career/components/career-table/career-table.component';
 import { ScrollClassDirective } from '@core/directives/scroll-class.directive';
+import { ExperienceService } from '@core/services/experience.service';
 
 @Component({
   selector: 'app-career',
@@ -14,11 +15,25 @@ import { ScrollClassDirective } from '@core/directives/scroll-class.directive';
   templateUrl: './career.component.html',
   styleUrl: './career.component.scss',
 })
-export class CareerComponent {
+export class CareerComponent implements OnInit {
   carrerIcon: Icon = carrerIcon;
   experiences: Experience[] = [];
 
   constructor() {
     this.experiences = experiencesData;
+  }
+
+  private readonly experienceService = inject(ExperienceService);
+
+  ngOnInit(): void {
+    this.getProjects();
+  }
+
+  public getProjects(): void {
+    this.experienceService.experiences.subscribe(
+      (experiences: Experience[]) => {
+        this.experiences = experiences;
+      }
+    );
   }
 }
